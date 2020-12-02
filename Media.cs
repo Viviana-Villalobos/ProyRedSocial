ba6b46f7a86a6a929d9dac95aa21395e1af6ddcc
erickdadice <48360309+erickdadice@users.noreply.github.com>
2020-12-01 21:05:45 -0600

    Add files via upload
    agregado de vistas, controlador de media

---
+++ Media.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace RedSocial.Models
+{
+    public class Media
+    {
+        [DisplayName("Identificación")]
+        public int IdMedia { get; set; }
+        [DisplayName("Nombre del Archivo")]
+        public string NombreFoto { get; set; }
+        [DisplayName("URL")]
+        public string ArchivoURL { get; set; }
+        [DisplayName("Nombre de la Foto")]
+        public string NomArchivoFoto { get; set; }
+        [DisplayName("Detalle de Media")]
+        public string DetalleFoto { get; set; }
+        [DisplayName("Id Usuario")]
+        public int IdUsuario { get; set; }
+    }
+}
